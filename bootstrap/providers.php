0a986bdf142add9ac107cd18b6c3aeada34246b8
popifra1409 <porio.peyie@gmail.com>
2026-03-24 10:54:15 +0100

    maj providers services

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -2,7 +2,7 @@
 
 return [
     App\Providers\AppServiceProvider::class,
-    App\Providers\Filament\AdminPanelProvider::class,
+    // App\Providers\Filament\AdminPanelProvider::class,
 
     App\Providers\Filament\PortalPanelProvider::class,
     App\Providers\Filament\BudgetPanelProvider::class,
