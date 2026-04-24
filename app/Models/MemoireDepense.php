516d62da3d2b33caec7404f9e88ee9de65cb1190
popifra1409 <porio.peyie>
2026-04-24 17:19:15 +0100

    correction du téléchargement MD

--- app/Models/MemoireDepense.php
+++ app/Models/MemoireDepense.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\SoftDeletes;
-use App\Services\NombreEnLettres;
+use App\Helpers\NombreEnLettres;
 use App\Traits\GereTransmissions;
 
 class MemoireDepense extends Model
